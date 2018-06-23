d20348d722fed66fe789797dac14ff7043fc7e12
slateding <mikezhangding@gmail.com>
2018-06-23 18:07:03 +0800

    jedies

--- platform-api/src/main/java/com/platform/dto/BuyGoodsDTO.java
+++ platform-api/src/main/java/com/platform/dto/BuyGoodsDTO.java
@@ -10,4 +10,32 @@ public class BuyGoodsDTO implements Serializable {
     private Integer productId;
     private Integer number;
     private String name;
+
+    public void setGoodsId(Integer goodsId) {
+        this.goodsId = goodsId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Integer getProductId() {
+        return productId;
+    }
+    public Integer getNumber() {
+        return number;
+    }
+    public Integer getGoodsId() {
+        return goodsId;
+    }
+    public String getName() {
+        return name;
+    }
 }
